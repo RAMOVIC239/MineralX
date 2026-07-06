ee0119f0796720c38277d2a4dfa5d792b8e88322
RAMOVIC239 <07ramovic@gmail.com>
2026-07-06 03:54:58 +0300

    Delete preloader code from script.js
    Removed preloader functionality from script.js.

--- js/script.js
+++ js/script.js
@@ -3,23 +3,6 @@ javascript
     MineralX Premium JavaScript
 ==========================================*/
 
-/*==============================
-    PRELOADER
-==============================*/
-
-window.addEventListener("load", function () {
-
-    const preloader = document.getElementById("preloader");
-
-    preloader.style.opacity = "0";
-
-    setTimeout(() => {
-
-        preloader.style.display = "none";
-
-    }, 500);
-
-});
 
 /*==============================
     STICKY NAVBAR
